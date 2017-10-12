6240a54289400404f34dad684d85355465410107
Jesse Atay <jesse.atay@gmail.com>
2017-10-11 17:51:41 -0700

    initial updates

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+
+gulp.task("default", function(){
+  console.log("default test complete");
+});
